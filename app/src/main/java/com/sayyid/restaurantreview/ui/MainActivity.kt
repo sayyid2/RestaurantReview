0d9e083b3f8048a569514642d9d2508acca8086f
sayyid <sayyidyaasiinalmahdi2@gmail.com>
2023-09-20 20:23:07 +0700

    Initial commit

---
+++ app/src/main/java/com/sayyid/restaurantreview/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sayyid.restaurantreview.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sayyid.restaurantreview.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
